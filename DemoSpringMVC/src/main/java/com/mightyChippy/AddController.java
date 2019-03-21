3a97118fd1fb76f57426385582d92488b2087f6c
UserPC <UserPC@HACHI>
2019-03-22 00:37:30 +0800

    Initial commit

---
+++ DemoSpringMVC/src/main/java/com/mightyChippy/AddController.java
@@ -0,0 +1,43 @@
+package com.mightyChippy;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AddController {
+
+	@Autowired
+	AddService addService;
+	
+	private int i;
+	private int j;
+	private int k;
+	
+	@RequestMapping("/add")
+	public ModelAndView add(HttpServletRequest request, HttpServletResponse response) {
+		
+		checkParameter(request);
+		execute();
+		
+		//preparing model to be passed in client
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("display.jsp");
+		mv.addObject("result", k);
+		
+		return mv;
+	}
+	
+	public void checkParameter(HttpServletRequest request) {
+		i = Integer.parseInt(request.getParameter("t1"));
+		j = Integer.parseInt(request.getParameter("t2"));
+	}
+	
+	public void execute() {
+		k = addService.add(i, j);
+	}
+}
---
+++ DemoSpringMVC/src/main/java/com/mightyChippy/AddService.java
@@ -0,0 +1,12 @@
+package com.mightyChippy;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AddService {
+
+	public int add(int i, int j) {
+		
+		return i + j;
+	}
+}
